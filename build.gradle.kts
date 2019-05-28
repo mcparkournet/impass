50e575aa56132aea381fb9af374b3efdda127e1a
Piotr Dobiech <pitcer@protonmail.com>
2019-05-28 21:31:18 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+	`java-library`
+}
+
+repositories {
+	jcenter()
+}
+
+dependencies {
+	compileOnly("org.jetbrains:annotations:17.0.0")
+	testImplementation("org.junit.jupiter:junit-jupiter-api:5.4.2")
+	testRuntime("org.junit.jupiter:junit-jupiter-engine:5.4.2")
+}
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_11
+	targetCompatibility = JavaVersion.VERSION_11
+}
+
+tasks {
+	test {
+		useJUnitPlatform {
+			includeEngines("junit-jupiter")
+		}
+	}
+}
