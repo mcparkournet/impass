ab691437a5424e27f977e61247f9efb29dc1aeb4
Piotr Dobiech <pitcer@protonmail.com>
2019-06-07 15:08:13 +0200

    Add bintray publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,16 @@
+import com.jfrog.bintray.gradle.BintrayExtension
+
 plugins {
 	`java-library`
+	`maven-publish`
+	id("com.jfrog.bintray") version "1.8.4" apply false
 }
 
 subprojects {
 	apply {
-		plugin("org.gradle.java-library")
+		plugin("java-library")
+		plugin("maven-publish")
+		plugin("com.jfrog.bintray")
 	}
 
 	repositories {
@@ -29,5 +35,37 @@ subprojects {
 				includeEngines("junit-jupiter")
 			}
 		}
+	}
+
+	task<Jar>("sourcesJar") {
+		from(sourceSets["main"].allSource)
+		archiveClassifier.set("sources")
+	}
+
+	publishing {
+		publications {
+			create<MavenPublication>("maven") {
+				from(components["java"])
+				artifact(tasks["sourcesJar"])
+			}
+		}
+	}
+
+	configure<BintrayExtension> {
+		user = properties["bintray-user"] as String?
+		key = properties["bintray-api-key"] as String?
+		publish = true
+		setPublications("maven")
+		pkg(closureOf<BintrayExtension.PackageConfig> {
+			repo = properties["mcparkour-bintray-repository"] as String?
+			userOrg = properties["mcparkour-bintray-organization"] as String?
+			name = project.name
+			desc = project.description
+			websiteUrl = "https://github.com/mcparkournet/impass"
+			issueTrackerUrl = "$websiteUrl/issues"
+			vcsUrl = "$websiteUrl.git"
+			setLicenses("MIT")
+			setLabels("java", "implementation-access", "proxy-class", "craftbukkit", "nms")
+		})
 	}
 }
